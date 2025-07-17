39735dbf3fd3de20d687c5812e1d08d172d6be27
LaboneeAktar <labonipub@gmail.com>
2025-07-17 19:33:00 +0600

    Added bookmark

--- src/Components/Bookmarks/Bookmarks.jsx
+++ src/Components/Bookmarks/Bookmarks.jsx
@@ -1,8 +1,15 @@
+import Bookmark from "../Bookmark/Bookmark";
+
 const Bookmarks = ({ bookmarks }) => {
   console.log(bookmarks);
   return (
-    <div className="md:w-1/3">
-      <h2 className="text-xl">Bookmarks: {bookmarks.length}</h2>
+    <div className="md:w-1/3 lg:ml-6 bg-slate-100 py-8 rounded-lg">
+      <h2 className="text-xl text-center font-semibold ">
+        Bookmarked Blogs: {bookmarks.length}
+      </h2>
+      {bookmarks.map((bookmark, idx) => (
+        <Bookmark key={idx} bookmark={bookmark}></Bookmark>
+      ))}
     </div>
   );
 };
